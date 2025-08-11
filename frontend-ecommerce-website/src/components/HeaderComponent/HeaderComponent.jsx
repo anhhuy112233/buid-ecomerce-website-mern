1ef9ef71552ec0f0db01a27c2e7d7ae6f66e2368
HyĐây^^ <hbui20352@gmail.com>
2025-08-11 15:22:36 +0700

    Initial commit

---
+++ frontend-ecommerce-website/src/components/HeaderComponent/HeaderComponent.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Col } from "antd";
+import Search from "antd/es/transfer/search";
+import {
+  WrapperHeader,
+  WrapperTextHeader,
+  WrapperHeaderAccount,
+  WrapperTextHeaderSmall,
+} from "./style";
+import {
+  UserOutlined,
+  CaretDownOutlined,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <WrapperHeader gutter={16}>
+        <Col span={6}>
+          <WrapperTextHeader>NOVATEACH</WrapperTextHeader>
+        </Col>
+        <Col span={12}>
+          <Search
+            placeholder="input search text"
+            allowClear
+            enterButton="Search"
+            size="large"
+          />
+        </Col>
+        <Col span={6} style={{ display: "flex", gap: "20px" }}>
+          <WrapperHeaderAccount>
+            <UserOutlined style={{ fontSize: "25px" }} />
+            <div>
+              <WrapperTextHeaderSmall>Đăng nhập/Đăng ký</WrapperTextHeaderSmall>
+              <div>
+                <WrapperTextHeaderSmall>Tài khoản</WrapperTextHeaderSmall>
+                <CaretDownOutlined />
+              </div>
+            </div>
+          </WrapperHeaderAccount>
+          <div>
+            <ShoppingCartOutlined style={{ fontSize: "25px", color: "#fff" }} />
+            <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
+          </div>
+        </Col>
+      </WrapperHeader>
+    </div>
+  );
+};
+
+export default HeaderComponent;
---
+++ frontend-ecommerce-website/src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../redux/slides/counterSlice'
+export const store = configureStore({
+  reducer: {
+     counter: counterReducer,
+  },
+})
