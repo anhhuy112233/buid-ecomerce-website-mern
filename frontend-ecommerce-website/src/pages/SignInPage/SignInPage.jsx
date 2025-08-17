e959f4ab527b67736b341d3bf84eae37436e6d43
HyĐây^^ <hbui20352@gmail.com>
2025-08-17 09:46:21 +0700

    Làm giao diện đăng nhập đăng ký tài khoản

--- frontend-ecommerce-website/src/pages/SignInPage/SignInPage.jsx
+++ frontend-ecommerce-website/src/pages/SignInPage/SignInPage.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from "react";
+
+import {
+  WrapperContainerLeft,
+  WrapperContainerRight,
+  WrapperTextLight,
+} from "./style";
+import InputForm from "../../components/InputForm/InputForm";
+import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
+import { LoginOutlined } from "@ant-design/icons";
+import { Image } from "antd";
+import imageLogo from "../../assets/images/tiki.png";
 
 const SignInPage = () => {
   return (
-    <div>SignInPage</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "rgba(0,0,0,0.53)",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          width: "800px",
+          height: "445px",
+          borderRadius: "6px",
+          backgroundColor: "#fff",
+        }}
+      >
+        <WrapperContainerLeft>
+          <h1>Xin chào!</h1>
+          <p>Đăng nhập hoặc tạo tài khoản</p>
+          <InputForm
+            style={{ marginBottom: "10px" }}
+            placeholder="abc@gmail.com"
+          />
+          <InputForm placeholder="password" />
+          <ButtonComponent
+            size="large"
+            style={{
+              backgroundColor: "#1a95ff",
+              color: "#fff",
+              borderRadius: "4px",
+              border: "none",
+              height: "48px",
+              width: "100%",
+              padding: "0 24px",
+              margin: "26px 0 10px",
+            }}
+            icon={<LoginOutlined />}
+          >
+            Đăng nhập
+          </ButtonComponent>
+          <p>
+            <WrapperTextLight>Quên mật khẩu?</WrapperTextLight>
+          </p>
+          <p>
+            Chưa có tài khoản <WrapperTextLight>Tạo tài khoản</WrapperTextLight>
+          </p>
+        </WrapperContainerLeft>
+        <WrapperContainerRight>
+          <Image
+            src={imageLogo}
+            preview={false}
+            alt="Image-Logo"
+            height="300px"
+            width="300px"
+          />
+          <h4>Mua sắm tại NovaTeach</h4>
+        </WrapperContainerRight>
+      </div>
+    </div>
+  );
+};
 
-export default SignInPage
+export default SignInPage;
--- frontend-ecommerce-website/src/pages/SignUpPage/SignUpPage.jsx
+++ frontend-ecommerce-website/src/pages/SignUpPage/SignUpPage.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
+import {
+  WrapperContainerLeft,
+  WrapperContainerRight,
+  WrapperTextLight,
+} from "./style";
+import InputForm from "../../components/InputForm/InputForm";
+import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
+import { LoginOutlined } from "@ant-design/icons";
+import { Image } from "antd";
+import imageLogo from "../../assets/images/tiki.png";
 
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "rgba(0,0,0,0.53)",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          width: "800px",
+          height: "445px",
+          borderRadius: "6px",
+          backgroundColor: "#fff",
+        }}
+      >
+        <WrapperContainerLeft>
+          <h1>Xin chào!</h1>
+          <p>Đăng ký tài khoản!</p>
+          <InputForm
+            style={{ marginBottom: "10px" }}
+            placeholder="abc@gmail.com"
+          />
+          <InputForm placeholder="password" style={{ marginBottom: "10px" }} />
+          <InputForm placeholder="confirm password" />
+          <ButtonComponent
+            size="large"
+            style={{
+              backgroundColor: "#1a95ff",
+              color: "#fff",
+              borderRadius: "4px",
+              border: "none",
+              height: "48px",
+              width: "100%",
+              padding: "0 24px",
+              margin: "26px 0 10px",
+            }}
+            icon={<LoginOutlined />}
+          >
+            Đăng ký
+          </ButtonComponent>
+          <p>
+            Bạn đã có tài khoản?{" "}
+            <WrapperTextLight>Đăng nhập tài khoản</WrapperTextLight>
+          </p>
+        </WrapperContainerLeft>
+        <WrapperContainerRight>
+          <Image
+            src={imageLogo}
+            preview={false}
+            alt="Image-Logo"
+            height="300px"
+            width="300px"
+          />
+          <h4>Mua sắm tại NovaTeach</h4>
+        </WrapperContainerRight>
+      </div>
+    </div>
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
